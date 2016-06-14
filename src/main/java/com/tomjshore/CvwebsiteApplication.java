1456271548c14b161145ad49545bc9ba33e37014
TomJShore <tomjshore@hotmail.co.uk>
2016-06-14 18:33:57 +0100

    hopfully added spring boot

---
+++ src/main/java/com/tomjshore/CvwebsiteApplication.java
@@ -0,0 +1,12 @@
+package com.tomjshore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvwebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvwebsiteApplication.class, args);
+	}
+}
