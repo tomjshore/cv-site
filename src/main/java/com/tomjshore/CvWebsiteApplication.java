57023cfad26eaf40c0b7fc9510b49f3314300ed1
TomJShore <tomjshore@hotmail.co.uk>
2016-06-14 20:51:34 +0100

    changed to a WAR

--- src/main/java/com/tomjshore/CvWebsiteApplication.java
+++ src/main/java/com/tomjshore/CvWebsiteApplication.java
@@ -1,14 +1,27 @@
 package com.tomjshore;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
-public class CvWebsiteApplication {
+@Configuration
+@EnableAutoConfiguration
+public class CvWebsiteApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(CvWebsiteApplication.class);
 		app.run(args);
 		
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(CvWebsiteApplication.class);
+	}
+	
+	
 }
