aa104aa480782fbb0d05f230537940a9f4aeb9da
tom <tomjshore@hotmail.co.uk>
2018-03-31 14:16:44 +0100

    fixed failing test cases in introController.js

--- frontend/test/spec/controllers/introcontroller.js
+++ frontend/test/spec/controllers/introcontroller.js
@@ -5,16 +5,20 @@ describe('Controller: IntrocontrollerCtrl', function () {
   // load the controller's module
   beforeEach(module('frontendApp'));
 
-  var IntrocontrollerCtrl, scope,$httpBackend,factory,defered;
+  var IntrocontrollerCtrl, scope,factory,defered, httpBackend;
   
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope,$injector,infoFactory,$q) {
+  beforeEach(inject(function ($controller, $rootScope,$injector,infoFactory,$q,$httpBackend) {
 		factory = infoFactory
 		scope = $rootScope.$new();
+		httpBackend = $httpBackend;
 
 		defered = $q.defer()
+		httpBackend.when('GET', 'views/main.html').respond('');
+
 
 		spyOn(factory,'getIntro').and.returnValue(defered.promise);
+		
 		var respond = {
 				data:{
 					intro: '<p>Some text</p>'
