b4bdfa768652884b7e00d9254a2b2caf70e48227
tom <tomjshore@hotmail.co.uk>
2018-04-28 15:31:52 +0100

    updated the spring boot version

--- src/main/java/com/tomjshore/CvWebsiteApplication.java
+++ src/main/java/com/tomjshore/CvWebsiteApplication.java
@@ -1,27 +1,13 @@
 package com.tomjshore;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
-@Configuration
-@EnableAutoConfiguration
-public class CvWebsiteApplication extends SpringBootServletInitializer {
+public class CvWebsiteApplication {
 
 	public static void main(String[] args) {
-		SpringApplication app = new SpringApplication(CvWebsiteApplication.class);
-		app.run(args);
-		
+		SpringApplication.run(CvWebsiteApplication.class, args);
 	}
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(CvWebsiteApplication.class);
-	}
-	
-	
 }
+
